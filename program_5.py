52645d37f7e5aef9e82ba0da622243c296fd261d
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-02-06 14:34:05 -0600

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -1,9 +1,6 @@
-# There are two kinds of hot dogs sold:  
-# Hot Dog ($3.50), Chili Dog ($4.50).  
-# Additionally a person can order cheese ($0.50), peppers ($0.75), or grilled onions ($1.00).  
-# Also tax is 7%. 
-# Write a program the inputs the type of hot dog wanted and optional toppings.  
-# The program then displays the hot dog cost, tax and total cost. 
+#Author: Sam Gaines
+#Date: 2/6/2026
+#Title: Hot Dog
 
 hot_dog_price= 3.50 
 chili_dog_price= 4.50
