23bbbbf11dd87bdd09daad88ad95b7252ab1bd00
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-02-06 13:38:05 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -8,7 +8,10 @@
 def categorize_age(age):
     ageCategory = "TBD"
     ######################
-    # WRITE YOUR CODE HERE
+    if age <=1: return "Infant"
+    elif 1< age <13: return "Child"
+    elif 13<= age < 20: return "Teenager"
+    else: return "Adult"
     ######################
 
 
