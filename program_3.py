06232c02408cfab51ebcb257cf2a7e18fd64c27a
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-02-06 14:02:42 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -11,15 +11,16 @@
 
 def weight_conversion(weight):
     # Calculate the shipping charge.
-    shippingCost = 0.0
+    
     ######################
     if weight <= 2:
-        price_per_pound = 1.50
-    elif weight <= 6: price_per_pound = 3.00
-    elif weight <= 10: price_per_pound = 4.00
-    else: price_per_pound = 4.75
-    shippingCost = price_per_pound
-    return shippingCost
+        return 1.50
+    elif weight <= 6:
+        return 3.00
+    elif weight <= 10:
+        return 4.00
+    else:
+        return 4.75
     ######################
     
 #### This piece of the code has been done for you,
